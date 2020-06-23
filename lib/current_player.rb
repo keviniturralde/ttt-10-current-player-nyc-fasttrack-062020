0fd70aa5cbf053d5f31ee33391632f2b37db9ff8
Kevin Iturralde <keviniturralde3@gmail.com>
2020-06-23 21:22:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,14 +5,12 @@ def turn_count(board)
       turns += 1
     end
   end
-  return turns
-end
 
 def current_player(board)
   turns = turn_count(board)
   if turns % 2 == 0
     retturn "X"
   else
-    return "X"
+    puts "X"
   end
 end
